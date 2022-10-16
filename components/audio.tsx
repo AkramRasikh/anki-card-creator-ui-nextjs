b1f436021f7b93e23008cfdb3fc2f53fc0863f36
Akram Rasikh <akram.rasikh@gmail.com>
2022-10-16 11:50:17 +0100

    remove event listener

--- components/audio.tsx
+++ components/audio.tsx
@@ -4,11 +4,36 @@ import styles from '../styles/Home.module.css';
 const Audio = ({ handleTimeUpdate, audioFile, startTime, endTime }) => {
   const audioRef = React.useRef(null);
   const snippet = startTime || endTime;
+
+  const internalHandleTimeUpdate = (evt) => {
+    const { currentTime } = evt.target;
+    const currentTimeIsBeforeStartTime = currentTime < startTime;
+    const currentTimeIsAfterEndTime = currentTime > endTime;
+
+    if (currentTimeIsBeforeStartTime) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = startTime;
+    } else if (currentTimeIsAfterEndTime) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = endTime;
+    }
+  };
+
   React.useEffect(() => {
     const el = audioRef.current;
     if (!snippet) {
       el.addEventListener('timeupdate', handleTimeUpdate);
+    } else {
+      el.addEventListener('timeupdate', internalHandleTimeUpdate);
     }
+
+    return () => {
+      if (!snippet) {
+        el.removeEventListener('timeupdate', handleTimeUpdate);
+      } else {
+        el.removeEventListener('timeupdate', internalHandleTimeUpdate);
+      }
+    };
   }, []);
 
   const source = !snippet
