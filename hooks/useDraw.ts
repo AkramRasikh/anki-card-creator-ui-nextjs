b663d0f0c5da229422801f9663ab4abd1392edcc
Akram Rasikh <akram.rasikh@gmail.com>
2022-10-17 18:35:31 +0100

    function image -> canvas & drawing

---
+++ hooks/useDraw.ts
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const useDraw = () => {
+  const [canvas, setCanvas] = React.useState(null);
+
+  let coord = { x: 0, y: 0 };
+
+  const start = (evt) => {
+    document.addEventListener('mousemove', draw);
+    document.addEventListener('mouseup', stop);
+    reposition(evt);
+  };
+  const draw = () => {
+    console.log('## draw?: ');
+    const ctx = canvas?.getContext('2d');
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = '#ACD3ED';
+    ctx.moveTo(coord.x, coord.y);
+    reposition(event);
+    ctx.lineTo(coord.x, coord.y);
+    ctx.stroke();
+  };
+
+  const stop = () => {
+    document.removeEventListener('mousemove', draw);
+  };
+
+  const reposition = (event) => {
+    if (canvas) {
+      coord.x = event.clientX - canvas?.offsetLeft;
+      coord.y = event.clientY - canvas?.offsetTop;
+    }
+  };
+
+  React.useEffect(() => {
+    const canvasEl = document?.getElementById('myCanvas');
+    setCanvas(canvasEl);
+    return () => {
+      document.removeEventListener('mousemove', draw);
+      document.removeEventListener('mouseup', stop);
+    };
+  }, []);
+
+  return start;
+};
+
+export default useDraw;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { nanoid } from 'nanoid';
 import Audio from '../components/audio';
+import useDraw from '../hooks/useDraw';
 
 export default function Home() {
   const [audioFile, setAudioFile] = React.useState(null);
   const [imageFile, setImageFile] = React.useState(null);
+  const [imageLoaded, setImageLoaded] = React.useState(false);
   const [currentAudioTime, setCurrentAudioTime] = React.useState();
   const [initStartTime, setInitStartTime] = React.useState();
   const [audioSnips, setAudioSnips] = React.useState([]);
 
   const canvasRef = React.useRef();
   const imageRef = React.useRef();
+  const start = useDraw();
 
   const handleAudioFileChange = (e) => {
     const file = e.target.files[0];
@@ -24,21 +27,32 @@ export default function Home() {
     setImageFile(url);
   };
 
-  React.useEffect(() => {
-    const condition = !!(imageFile && imageRef?.current?.height);
-    if (condition) {
-      const canvas = document.getElementById('myCanvas');
-      const image = document.getElementById('initImage');
-      image.style.display = 'none';
-      const ctx = canvas.getContext('2d');
-      const imageHeight = imageRef.current.height;
-      const imageWidth = imageRef.current.width;
+  const canvasCreation = ({ imageRef, image }) => {
+    const canvas = document.getElementById('myCanvas');
+    const ctx = canvas.getContext('2d');
+    const imageHeight = imageRef.current.height;
+    const imageWidth = imageRef.current.width;
+    canvas.width = imageWidth;
+    canvas.height = imageHeight;
+    ctx.drawImage(image, 0, 0);
+    document.addEventListener('mousedown', start);
+  };
 
-      canvas.width = imageWidth;
-      canvas.height = imageHeight;
-      ctx.drawImage(image, 0, 0);
+  React.useEffect(() => {
+    const image = document?.getElementById('initImage');
+    if (imageLoaded) {
+      image.style.display = 'none';
+      canvasCreation({ imageRef, image });
     }
-  }, [imageFile, imageRef]);
+    if (image && !imageLoaded) {
+      image.onload = function () {
+        setImageLoaded(true);
+      };
+    }
+    return () => {
+      document.removeEventListener('mousedown', start);
+    };
+  }, [imageFile, imageRef, canvasRef, imageLoaded]);
 
   const handleTimeUpdate = (evt) => {
     setCurrentAudioTime(evt.target.currentTime);
