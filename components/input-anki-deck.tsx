9bb872e2397c041dd40734abd9965a7618e68681
Akram Rasikh <akram.rasikh@gmail.com>
2022-11-12 18:13:27 +0000

    input anki name

--- components/input-anki-deck.tsx
+++ components/input-anki-deck.tsx
@@ -1,7 +1,14 @@
-const InputAnkiDeckName = ({ handleChange, value }) => {
-  <input
-    type='text'
-    onChange={(e) => handleChange(e.target.value)}
-    value={value}
-  />;
-};
+import React from 'react';
+
+const InputAnkiDeckName = ({ handleChange, value }) => (
+  <div>
+    <span>Anki deck name:</span>
+    <input
+      type='text'
+      onChange={(e) => handleChange(e.target.value)}
+      value={value}
+    />
+  </div>
+);
+
+export default InputAnkiDeckName;
